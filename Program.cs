38d519483e24a14b021c7eb90dc886e5e8975d03
Ксения <weberxks@gmail.com>
2025-04-22 19:42:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SpamDetector;
+
+
+//Load sample data
+var sampleData = new Spam.ModelInput()
+{
+    Subject = @"Meeting tomorrow at 10 AM",
+    Content = @"Hi team, Just a reminder that we have our weekly project status meeting tomorrow at 10 AM in Conference Room B. Please bring your status reports and be prepared to discuss the timeline updates. Thanks, Sarah",
+};
+
+//Load model and predict output
+var result = Spam.Predict(sampleData);
+
+
+Console.WriteLine(result.PredictedLabel);
